c8ef19f0008a5af8287109ffa900f0c46e8059cd
ayushmaan162 <ayushmaans718@gmail.com>
2024-09-15 18:53:06 +0100

    dodgeball.py

--- dodgeball/dodgeball.py
+++ dodgeball/dodgeball.py
@@ -21,6 +21,7 @@ wnf=pygame.font.SysFont('Alberobelloserif',50)
 p1=pygame.image.load("dodgeball p1.png")
 p2=pygame.image.load("dodgeball p2.png")
 arena=pygame.transform.scale(pygame.image.load("dodgeball arena.jpg"),(WIDTH,HEIGHT))
+border=pygame.Rect(350,0,10,500)
 h1=5
 h2=5
 class pm(pygame.sprite.Sprite):
@@ -42,16 +43,61 @@ class pm(pygame.sprite.Sprite):
         self.rect.move_ip(0,vel)
         if self.rect.top<=0 or self.rect.bottom>=500:
             self.rect.move_ip(0,-vel)
-pl1=pm(p1,0,100,200)
-pl2=pm(p2,0,600,200)
+pl1=pm(p1,0,100,350)
+pl2=pm(p2,0,600,350)
 sprites=pygame.sprite.Group()
 sprites.add(pl1)
 sprites.add(pl2)
 run=True
+def bg():
+    Screen.blit(arena,(0,0))
+    pygame.draw.rect(Screen,"red",border)
+    pl1text=hf.render("Health:"+str(h1),1,"green")
+    pl2text=hf.render("Health:"+str(h2),2,"green")
+    Screen.blit(pl1text,(100,10))
+    Screen.blit(pl2text,(460,10))
+pl1ball=[]
+pl2ball=[]   
+def bal():
+    for ball in pl1ball:
+        pygame.draw.circle(Screen,"red",(ball.x,ball.y),10)
+        ball.x-=5
+    for ball in pl2ball:
+        pygame.draw.circle(Screen,"yellow",(ball.x,ball.y),10)
+        ball.x-=5
+pl1_hit=pygame.USEREVENT+1
+pl2_hit=pygame.USEREVENT+2
 while run:
     for event in pygame.event.get():
         if event.type == QUIT:
             run = False
+        if event.type==KEYDOWN:
+            if event.key==K_LSHIFT:
+                ball=(pl1.rect.x+pl1.rect.width,pl1.rect.y+pl1.rect.height//2)
+                pl1ball.append(ball)
+            if event.key==K_RSHIFT:
+                ball=(pl2.rect.x,pl2.rect.y+pl2.rect.height//2)
+                pl2ball.append(ball)
+    bg()
     sprites.draw(Screen)
+    bal()
+    key=pygame.key.get_pressed()
+    if key[K_a]:
+        pl1.horizontal(-1,1)
+    if key[K_d]:
+        pl1.horizontal(1,1)
+    if key[K_w]:
+        pl1.vertical(-1)
+    if key[K_s]:
+        pl1.vertical(1)
+    #player2
+    if key[K_LEFT]:
+        pl2.horizontal(-1,2)
+    if key[K_RIGHT]:
+        pl2.horizontal(1,2)
+    if key[K_UP]:
+        pl2.vertical(-1)
+    if key[K_DOWN]:
+        pl2.vertical(1)
     pygame.display.update()
 pygame.quit()
